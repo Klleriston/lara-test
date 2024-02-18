0d567adfc3ec78a98f2404dbe1554a5824327308
klleriston <klleandrade@outlook.com>
2024-02-17 23:04:02 -0300

    tests

---
+++ lara4nubs/app/Models/Post.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $table = 'posts';
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'published'
+    ];
+
+    protected $casts = [
+        'published' => 'boolean'
+    ];
+    
+}
